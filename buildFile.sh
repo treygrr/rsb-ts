48cbddbc04404d6dbbc78594131350b580519c39
Trey <phlasc@gmail.com>
2022-01-21 21:49:40 -0500

    Build

--- buildFile.sh
+++ buildFile.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -i
 echo $PATH
 echo "Node version is $(node -v)"
+# run yarn build and pipe the output to terminal
+yarn build
+# restart pm2 and wait for it to be ready
+pm2 restart all
+
+echo "Build complete"
+
 exit 0
