681331fc55768d06b189fce5f0aec51738848fba
Trey <phlasc@gmail.com>
2022-01-22 15:14:18 -0500

    Test data

--- src/common/ImageGenerator/main.ts
+++ src/common/ImageGenerator/main.ts
@@ -1,7 +1,18 @@
+import { ItemData } from "../../Interfaces/SearchInterfaces.js";
 import PlayWripper from "../PlayWripper/PlayWripper.js";
 import ImageGenerator from "./ImageGenerator.js";
 const browser = new PlayWripper();
-const image = new ImageGenerator();
+
+const itemData: ItemData = {
+  image: 'https://i.imgur.com/Z5XQZQW.png',
+  name: 'Dragon',
+  id: '12345',
+  members: true,
+  price: '$1.00',
+  change: '+1.00',
+}
+
+const image = new ImageGenerator(itemData, 'Test');
 
 
 
