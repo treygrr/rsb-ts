15749be637095d529f49c060ffc672edf8335ac7
Trey <phlasc@gmail.com>
2022-01-21 22:08:11 -0500

    tsatw

--- buildFile.sh
+++ buildFile.sh
@@ -2,9 +2,9 @@
 echo $PATH
 echo "Node version is $(node -v)"
 # run yarn build and pipe the output to terminal
-yarn build
+echo 'yarn build'
 # restart pm2 and wait for it to be ready
-pm2 restart all
+echo 'pm2 restart all'
 
 echo "Build complete"
 
