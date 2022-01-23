da6d7dcfdef48c5d5726fcbeb8f0387ca756bb1a
Trey <phlasc@gmail.com>
2022-01-23 00:38:10 -0500

    Update testing file

--- src/common/ImageGenerator/main.ts
+++ src/common/ImageGenerator/main.ts
@@ -1,6 +1,8 @@
 import { ItemData } from "../../Interfaces/SearchInterfaces.js";
+import GrandExchangeSearch from "../GrandExchangeSearch/GrandExchangeSearch.js";
 import PlayWripper from "../PlayWripper/PlayWripper.js";
 import ImageGenerator from "./ImageGenerator.js";
+import { search } from "../PowerSearch/PowerSearch.js";
 const browser = new PlayWripper();
 
 const itemData: ItemData = {
@@ -12,11 +14,23 @@ const itemData: ItemData = {
   change: '+1.00',
 }
 
-const image = new ImageGenerator(itemData, 'Test');
+const newStart = performance.now();
+const data = new GrandExchangeSearch();
+const searchResults = await data.search({ searchQuery: 'dragon sword', disableAll: true });
+if (searchResults) {
+  const image = new ImageGenerator(searchResults.matchedResults[0], 'Test');
+  await image.generateImage();
+}
+const newEnd = performance.now();
+// console log the time to generate in seconds
+console.log(`Time to generate same result on new system: ${(newEnd - newStart) / 1000} seconds`);
 
+const oldStart = performance.now();
+await search('dragon sword');
+const oldEnd = performance.now();
+// console log the time to generate in seconds
+console.log(`Time to generate same result on old system: ${(oldEnd - oldStart) / 1000} seconds`);
 
-
-console.log('I cry erytym');
 
 
 
