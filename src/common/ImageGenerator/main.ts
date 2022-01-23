4df3192c754e77e45115105cd3c10a58ebaf6ceb
Trey <phlasc@gmail.com>
2022-01-23 01:15:41 -0500

    Add logic to exit browser on interrupt

--- src/common/ImageGenerator/main.ts
+++ src/common/ImageGenerator/main.ts
@@ -2,6 +2,7 @@ import { ItemData } from "../../Interfaces/SearchInterfaces.js";
 import GrandExchangeSearch from "../GrandExchangeSearch/GrandExchangeSearch.js";
 import PlayWripper from "../PlayWripper/PlayWripper.js";
 import ImageGenerator from "./ImageGenerator.js";
+
 const browser = new PlayWripper();
 
 const itemData: ItemData = {
@@ -13,8 +14,16 @@ const itemData: ItemData = {
   change: '+1.00',
 }
 
+const data = new GrandExchangeSearch({ oldschool: true });
+
 const searchResults = await data.search({ searchQuery: 'dragon sword', disableAll: true });
+
 if (searchResults) {
+  const image = new ImageGenerator(searchResults, 'ResultsTable');
   await image.generateImage();
 }
 
+// when process.exit is called, the browser will close
+process.on('exit', async () => {
+  await browser.browser.close();
+});
